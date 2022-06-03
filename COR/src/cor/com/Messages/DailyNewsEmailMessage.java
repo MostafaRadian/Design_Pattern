28588403e61b514767c5436204bdc297e4a468ff
Mostafa A. Radian <mostafa.batesta@gmail.com>
2022-06-03 20:14:58 +0200

    Delete COR directory

--- COR/src/cor/com/Messages/DailyNewsEmailMessage.java
+++
@@ -1,9 +0,0 @@
-package cor.com.Messages;
-
-public class DailyNewsEmailMessage {
-	
-	public String prepareMessage(String placeHolders[]) {
-		// code to replace place holders of this type
-		return "";
-	}
-}
--- COR/src/cor/com/handler.java
+++
@@ -1,9 +0,0 @@
-package cor.com;
-
-import cor.com.Users.users;
-
-public interface handler {
-    void setNext(baseHandler next);
-
-    void handle(String request);
-}
